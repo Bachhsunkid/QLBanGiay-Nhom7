bc1cbec21f816160e1616d6752ef21bce823376a
NinhUTCK61 <tranquangninh321.work@gmail.com>
2023-03-01 21:16:00 +0700

    create login, regiter

---
+++ Nhom8.QuanlyBanGiay.Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Nhom8.QuanlyBanGiay.Admin.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
